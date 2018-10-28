86412510be9dba55ef33d135d8932d8267b602f3
Balou9 <emmanuel.owosekun@gmail.com>
2018-10-28 22:37:43 +0100

    new sma fl"

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function sumr (ts, end) {
-  var sum = 0
-  var count = (end-1) - 4
-  do {
-    sum += ts[count]
-    count++
-  } while (count < end)
+function sum (arr, start, end) {
+  for (var sum = 0, i = start; i < end; i++) {
+    sum += arr[i]
+  }
   return sum / end
 }
 
-
 function sma (ts, end) {
   var ts_len = ts.length
-  const sma = Array(ts_len).fill(null)
-  return sma
+  var sma = Array(ts_len).fill(null)
+  var start = 0
+  do {
+     sma[end-1] = sum(ts, start, end)
+     start++
+     end++
+  } while (end < ts_len)
+  return {start, end, ts_len, sma}
 }
 
-module.exports = { sma, sumr }
+module.exports = { sma, sum }
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const tape = require('tape')
-const { sma, sumr } = require('./index.js')
+const {sma, sum} = require('./index.js')
 const ts = [11, 88, 23, 45, 23,
             55, 66, 87, 32, 45,
             67, 65, 23, 47, 94]
@@ -9,7 +9,8 @@ tape('sma true', t => {
   t.end()
 })
 
-tape('sumr true', t => {
-  t.true(sumr(ts, 5))
+tape('sum true', t => {
+  t.true(sum([1,2,3,4,5,6,7,8], 0, 5) === 3)
+  t.true(sum([2,4,6,6,7,6,7,8], 0, 3) === 4)
   t.end()
 })
