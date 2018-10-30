8a2ccd0008d74ddb028f082cf0245049a64a4622
Balou9 <emmanuel.owosekun@gmail.com>
2018-10-30 18:47:32 +0100

    rm tslen

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function sum (arr, start, end) {
 }
 
 function sma (ts, end) {
-  var ts_len = ts.length
-  var sma = Array(ts_len).fill(null)
+  var sma = Array(ts.length).fill(null)
   var start = 0
   var i = end - 1
   var d = end
@@ -16,7 +15,7 @@ function sma (ts, end) {
      i++
      start++
      end++
-  } while (i !== ts_len)
+  } while (i !== ts.length)
   return sma
 }
 
